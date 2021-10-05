2c980d892427335672f78de5d55a9ef796ddd6c9
jrober50 <jrober50@vols.utk.edu>
2021-10-05 12:19:15 -0400

    Finishing Integrator_3D.f90 fixes

--- Code/Integrators/Integrator_3D.f90
+++ Code/Integrators/Integrator_3D.f90
@@ -28,11 +28,20 @@ USE constants, &
 
 
 USE Global_Variables_And_Parameters, &
-            ONLY :  NUM_R_ELEMENTS, NUM_P_ELEMENTS, NUM_T_ELEMENTS,     &
-                    rlocs, plocs, tlocs,                                &
-                    Source_Function_Flag, NUM_R_NODES, L_LIMIT, DEGREE, &
-                    Source_Vector, Source_Function, Discontinuity_Flag, &
-                    Source_Degrees, Source_Term_Coefficients
+            ONLY :  NUM_R_ELEMENTS,             &
+                    NUM_P_ELEMENTS,             &
+                    NUM_T_ELEMENTS,             &
+                    rlocs, plocs, tlocs,        &
+                    Source_Function_Flag,       &
+                    NUM_R_NODES,                &
+                    L_LIMIT,                    &
+                    DEGREE,                     &
+                    Source_Vector,              &
+                    Source_Function,            &
+                    Discontinuity_Flag,         &
+                    Source_Degrees,             &
+                    Source_Term_Coefficients,   &
+                    Source_Terms
 
 USE Additional_Functions_Module, &
             ONLY :  Legendre_Poly, Lagrange_Poly,   &
@@ -66,8 +75,8 @@ COMPLEX(KIND = idp), DIMENSION(0:NUM_R_NODES-1, -L_LIMIT:L_LIMIT, 0:L_LIMIT), IN
 INTEGER                                             ::  l, m, re, rd, te, td, pe, pd, p
 
 
-REAL(KIND = idp)                                    ::  deltar, deltap, deltat,     &
-                                                        SphereHarm_NormFactor
+REAL(KIND = idp)                                    ::  drot, dtot, dpot
+REAL(KIND = idp)                                    ::  SphereHarm_NormFactor
 
 REAL(KIND = idp), ALLOCATABLE, DIMENSION(:)         ::  R_xlocs, R_locs, R_weights, &
                                                         P_xlocs, P_locs, P_weights, &
@@ -78,9 +87,18 @@ REAL(KIND = idp), ALLOCATABLE, DIMENSION(:)         ::  R_xlocs, R_locs, R_weigh
 REAL(KIND = idp), DIMENSION(0:DEGREE)               ::  Poly_xlocs, Poly_weights, LagP
 
 
+INTEGER                                             ::  There, Here
+
 REAL(KIND = idp)                                    ::  R_SQR, T_PRE
 COMPLEX(KIND = idp)                                 ::  P_PRE
 
+REAL(idp),DIMENSION(1:Source_Degrees(1))            ::  R_SQR_Array
+REAL(idp),DIMENSION(1:Source_Degrees(2))            ::  T_Pre_Array
+COMPLEX(idp), DIMENSION(1:Source_Degrees(3))        ::  P_Pre_Array
+
+REAL(idp),DIMENSION(0:Degree,1:Source_Degrees(1))   ::  LagP_Array
+
+COMPLEX(idp), DIMENSION(1:Source_Degrees(1)*Source_Degrees(2)*Source_Degrees(3))        :: Int_Term
 
 
 ALLOCATE(R_xlocs(1:Source_Degrees(1)), R_locs(1:Source_Degrees(1)), R_weights(1:Source_Degrees(1)))
@@ -105,30 +123,35 @@ CALL Initialize_LG_Quadrature(Source_Degrees(3), T_xlocs, T_weights)
 P_locs = Map_From_X_Space(0.0_idp, 2*pi, P_xlocs)
 T_locs = Map_From_X_Space(0.0_idp, pi, T_xlocs)
 
-deltap = 2.0_idp * pi
-deltat = pi
 
 Src_Array = 0.0_idp
 
+DO pe = 0,NUM_P_ELEMENTS - 1
+DO te = 0,NUM_T_ELEMENTS - 1
+DO re = 0,NUM_R_ELEMENTS - 1
+
 DO l = 0, L_LIMIT
 DO m = -l, l
-DO re = 0,NUM_R_ELEMENTS - 1
-DO te = 0,NUM_T_ELEMENTS - 1
-DO pe = 0,NUM_P_ELEMENTS - 1
+DO p = 0,DEGREE
 
+
+
+
+    There = re*Degree + p
 
     R_locs = Map_From_X_Space(rlocs(re),rlocs(re+1), R_xlocs)
     T_locs = Map_From_X_Space(tlocs(te), tlocs(te+1), T_xlocs)
     P_locs = Map_From_X_Space(plocs(pe), plocs(pe+1), P_xlocs)
 
 
-    deltar = (rlocs(re+1) - rlocs(re))
-    deltat = tlocs(te+1)-tlocs(te)
-    deltap = plocs(pe+1) - plocs(pe)
+    drot = 0.5_idp *(rlocs(re+1) - rlocs(re))
+    dtot = 0.5_idp *(tlocs(te+1) - tlocs(te))
+    dpot = 0.5_idp *(plocs(pe+1) - plocs(pe))
 
 
     SphereHarm_NormFactor = POWER(-1.0_idp, m)*Norm_Factor(l,-m)
     SphereHarm_ThetaPart = SphereHarm_NormFactor*Legendre_Poly(l, -m, Source_Degrees(2), T_locs)
+
 
 
     DO rd = 1, Source_Degrees(1)
@@ -136,25 +159,23 @@ DO pe = 0,NUM_P_ELEMENTS - 1
     DO pd = 1, Source_Degrees(3)
 
         LagP = Lagrange_Poly(R_xlocs(rd), DEGREE, Poly_xlocs)
-        R_SQR = R_locs(rd)*R_locs(rd)*deltar/2.0_idp * R_weights(rd)
-        P_PRE = CDEXP(complex(0.0_idp,-m * P_locs(pd))) * deltap/2.0_idp * P_weights(pd)
 
-        T_PRE = sin(T_locs(td))* SphereHarm_ThetaPart(td) * deltat/2.0_idp * T_weights(td)
-
-!        PRINT*,re,rd,td,pd,R_SQR,P_PRE,T_PRE,Source_Term_Coefficients(re,te,pe,rd,td,pd)
-
-        DO p = 0,DEGREE
+        R_SQR = 4.0_idp * pi * R_locs(rd)*R_locs(rd)*drot * R_weights(rd)
+        T_PRE = sin(T_locs(td))* SphereHarm_ThetaPart(td) * dtot * T_weights(td)
+        P_PRE = CDEXP(complex(0.0_idp,-m * P_locs(pd))) * dpot * P_weights(pd)
 
 
-
-            Src_Array(re*DEGREE + p, m, l) = Src_Array(re*DEGREE + p, m, l)                     &
-                                                - Source_Term_Coefficients(re,te,pe,rd,td,pd)   &
-                                                * 4.0_idp * pi * R_SQR*LagP(p)                  &
-                                                * T_PRE                                         &
-                                                * P_PRE
+        Here = (pd-1)*Source_Degrees(2)*Source_Degrees(1)   &
+             + (td-1)*Source_Degrees(1)                     &
+             +  rd
 
 
-        END DO ! p Loop
+        Src_Array(There, m, l) = Src_Array(There, m, l)         &
+                                - Source_Terms(Here,re,te,pe)   &
+                                * R_SQR                         &
+                                * T_PRE                         &
+                                * P_PRE                         &
+                                * LagP(p)
 
     END DO ! pd Loop
     END DO ! td Loop
@@ -162,6 +183,7 @@ DO pe = 0,NUM_P_ELEMENTS - 1
 
 END DO  ! pe Loop
 END DO  ! te Loop
+END DO ! p Loop
 END DO  ! re Loop
 END DO  ! m Loop
 END DO  ! l Loop
